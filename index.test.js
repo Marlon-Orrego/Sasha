8d9a4137ede37125ba84c889ea59cbcf9a97b0e8
Marlon-Orrego <55565109+Marlon-Orrego@users.noreply.github.com>
2022-10-03 23:11:00 -0500

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,21 +1,8 @@
-
-
-const ValidaEmail = require('./allTest')
-
- 
-
+const ValidaEmail = require('./test.js')
 //1
-
 test('retorna true para input "user@gmail.com"', () => {
-
    expect(ValidaEmail.isValidarEmail('user@gmail.com')).toBe(true);
-
 });
-
- 
-
 test('retorna false para input "a"', () => {
-
     expect(ValidaEmail.isValidarEmail('a')).toBe(false);
-
 });
