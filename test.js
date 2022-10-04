a041c150dfc7e932870663b7bdf3436cbbad92cb
Marlon-Orrego <55565109+Marlon-Orrego@users.noreply.github.com>
2022-10-03 23:17:46 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,18 +1,7 @@
-
-
-
 class ValidaEmail{
-
     isValidarEmail(email){
-
       const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-
       return re.test(String(email).toLowerCase());
-
     }
-
   }
-
- 
-
   module.exports = new ValidaEmail();
